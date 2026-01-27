54f97d3e172e9fd822fb5a25b5ee9d253a85ebe5
AidenTing <aidentinghk@gmail.com>
2026-01-27 21:15:32 +0800

    Refactor main.py for improved clarity and structure

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ def main():
 #Step 1: Show Keygen works by printing (ECC)
     alice = KeyPair()
     bob = KeyPair()
-
+    print("=== Step 1:Key Generation (ECC) ===")
+    print()
     print("=== Alice Keys ===")
     print("Private Key:", alice.get_private_key_hex())
     print("Public Key :", alice.get_public_key_hex())
@@ -20,41 +21,42 @@ def main():
     print()
 
 #Step 2: Create the BlockChain
+    print("=== Step 2: Blockchain Creation ===")
     blockchain = Blockchain()
+    print("blockchain created with genesis block.")
+    print()
 
 #Step 3: Create a Trasnsaction
+    print("=== Step 3: Create Transaction ===")
     tx1 = Transaction(alice.public_key, bob.public_key, 10)
-    print("=== Transaction ===")
     print("Sender  : Alice")
     print("Receiver: Bob")
     print("Amount  : 10 BTC")
     print()
 
 #Step 4: Sign Transaction 
-    print("=== Signing Transaction ===")
+    print("=== Step 4: Signing Transaction (ECDSA) ===")
     tx1.sign_transaction(alice.private_key)
     print("Transaction Signature:", tx1.signature.hex())
+    print()
 
 #Step 5: Verify Signature
+    print("=== Step 5: Verify Signature ===")
     print("Signature valid:", tx1.is_valid())
     print()
 
 #Step 6: Tampering Transaction
-    print("=== Transaction Hashing (SHA-256) ===")
+    print("=== Step 6: Transaction Tampering (SHA-256) ===")
     original_hash = hashlib.sha256(tx1.transaction_data()).hexdigest()
-    print("Original Hash :", original_hash)
-    print()
-
     tx1.amount = 999
     tampered_hash = hashlib.sha256(tx1.transaction_data()).hexdigest()
-    print("=== Tampering Transaction ===")
     print("Original Hash :", original_hash)
     print("Tampered Hash :", tampered_hash)
     print("Signature valid after tampering:", tx1.is_valid())
     print()
 
 #Step 7: Fix Tampered Transaction
-    print("=== Re-signing After Fix ===")
+    print("=== Step 7: Re-signing After Fix ===")
     tx1.amount = 10
     print("Old signature valid:", tx1.is_valid())
     tx1.sign_transaction(alice.private_key)
@@ -63,19 +65,19 @@ def main():
     print()
 
 #Step 8: Mining Block
-    print("=== Mining Block (Proof of Work) ===")
+    print("=== Step 8: Mining Block (Proof of Work) ===")
     print("Mining block...")
     blockchain.add_block([tx1])
     print("Block successfully mined")
     print()
 
 #Step 9: Blockchain Validation
-    print("=== Blockchain Validation ===")
+    print("=== Step 9: Blockchain Validation ===")
     print("Blockchain valid:", blockchain.is_chain_valid())
     print()
 
-#Step 10: BLockchain Tampering
-    print("=== Tampering Blockchain ===")
+#Step 10: Blockchain Tampering
+    print("=== Step 10: Tampering Blockchain ===")
     blockchain.chain[1].transactions[0].amount = 999
     print("Blockchain valid after tampering:", blockchain.is_chain_valid())
     print()
